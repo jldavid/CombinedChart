b668900e3167c95e819b94144d1d34a0c589bcb2
jldavid <jldavid.mail@gmail.com>
2025-02-09 23:36:57 -0500

    Initial commit

---
+++ CombinedCharts/CombinedChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombinedChartsApp.swift
+//  CombinedCharts
+//
+//  Created by JL David on 2025-02-05.
+//
+
+import SwiftUI
+
+@main
+struct CombinedChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
